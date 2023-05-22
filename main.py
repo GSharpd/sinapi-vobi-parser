12be0580ceea47f8c3ff073fb98d109d2987be66
Gustavo Machado Torres Nunes Vieira <67484701+GSharpd@users.noreply.github.com>
2023-05-22 15:56:33 -0300

    fix number conversion

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ for filename in os.listdir(input_directory):
         # Load spreadsheet skipping the first 5 rows as they do not contain column names
         xls_file = pd.read_excel(input_filepath, skiprows=5)
         
-        # Filter out blank rows
-        xls_file = xls_file[xls_file['CODIGO DA COMPOSICAO'].notna()]
+        # # Filter out blank rows
+        # xls_file = xls_file[xls_file['CODIGO DA COMPOSICAO'].notna()]
 
         # Prepare a dictionary to apply the mapping
         mapping = {
@@ -30,7 +30,7 @@ for filename in os.listdir(input_directory):
                             np.where(xls_file['TIPO ITEM'] == 'INSUMO', 'Produto', 
                                     np.where(xls_file['TIPO ITEM'] == 'COMPOSICAO', 'Serviço', xls_file['TIPO ITEM']))),
             "Nome": np.where(xls_file['TIPO ITEM'].isnull() | xls_file['TIPO ITEM'].eq(''), xls_file['DESCRICAO DA COMPOSICAO'], xls_file['DESCRIÇÃO ITEM']),
-            "Quantidade": xls_file['COEFICIENTE'],
+            "Quantidade": pd.to_numeric(xls_file['COEFICIENTE'].str.replace(',', '.')), # replace comma with period and convert to numeric
             "Un": np.where(xls_file['UNIDADE ITEM'].isnull() | xls_file['UNIDADE ITEM'].eq(''), xls_file['UNIDADE'], xls_file['UNIDADE ITEM']),
             "Custo unitário": xls_file['PRECO UNITARIO'],
             "Classe": np.where(xls_file['TIPO ITEM'].isnull() | xls_file['TIPO ITEM'].eq(''), xls_file['SIGLA DA CLASSE'], ''),
